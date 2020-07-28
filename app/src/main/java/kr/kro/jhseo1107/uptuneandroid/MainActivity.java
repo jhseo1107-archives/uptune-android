a354409267c9c03d751ee175e6485f071045832c
Janghyub Seo <seojanghyeob@gmail.com>
2020-07-28 14:42:35 +0900

    Project Initialization

---
+++ app/src/main/java/kr/kro/jhseo1107/uptuneandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.kro.jhseo1107.uptuneandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
