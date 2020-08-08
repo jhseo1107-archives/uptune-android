6bb2ccbce3de4e92ecdeea36a9eaeda4831d7457
Janghyub Seo <seojanghyeob@gmail.com>
2020-08-09 00:18:53 +0900

    ClassVideoView

--- app/src/main/java/kr/kro/uptune/uptuneandroid/ClassVideoViewActivity.kt
+++ app/src/main/java/kr/kro/uptune/uptuneandroid/ClassVideoViewActivity.kt
@@ -2,11 +2,24 @@ package kr.kro.uptune.uptuneandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.widget.ProgressBar
 
 class ClassVideoViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_class_video_view)
+
+        var videoProgressBar = findViewById(R.id.videoProgressBar) as ProgressBar
+
+        videoProgressBar.visibility = View.GONE
+
+        var html = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/hMG1cg8wBmM\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>"
+        var videoWebView = findViewById(R.id.videoWebView) as WebView
+
+        videoWebView.settings.javaScriptEnabled = true
+        videoWebView.loadData(html, "text/html", null)
     }
 }
