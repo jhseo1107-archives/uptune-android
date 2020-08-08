e8721c0f16b850f18cab52e7546e78670e0f6041
Janghyub Seo <seojanghyeob@gmail.com>
2020-08-08 23:51:35 +0900

    ClassListActivity

---
+++ app/src/main/java/kr/kro/uptune/uptuneandroid/ClassVideoViewActivity.kt
@@ -0,0 +1,12 @@
+package kr.kro.uptune.uptuneandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClassVideoViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_class_video_view)
+    }
+}
--- app/src/main/java/kr/kro/uptune/uptuneandroid/ShowClassActivity.kt
+++ app/src/main/java/kr/kro/uptune/uptuneandroid/ShowClassActivity.kt
@@ -1,12 +1,34 @@
 package kr.kro.uptune.uptuneandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 
 class ShowClassActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_class)
+
+        var classListProgressBar = findViewById(R.id.classListProgressBar) as ProgressBar
+
+        classListProgressBar.visibility = View.GONE
+    }
+
+    fun onClassVideoClick(view : View)
+    {
+        changeScreen(ClassVideoViewActivity::class.java)
+    }
+
+    fun makeToast(content: String, type: Int) {
+        Toast.makeText(this.applicationContext, content, type).show()
+    }
+
+    fun changeScreen(activity: Class<*>) {
+        var intent = Intent(this, activity)
+        startActivity(intent)
     }
 }
